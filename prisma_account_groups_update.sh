07b84c28605f49c5dab9d23817492f168c7c5253
andywoodt2 <90482206+andywoodt2@users.noreply.github.com>
2022-04-30 14:44:43 +0000

    Reordering if variables.
    Reordered the ACCOUNTIDS and DESCRIPTION variables to earlier in the script.  Also change the "if -f to if -s, to ensure the file size is not zero.

--- prisma_account_groups_update.sh
+++ prisma_account_groups_update.sh
@@ -4,7 +4,7 @@
 #  I'm not a fan of using "-k" in the cURL commands, but hey, it's cURL, the certs are EC, and cURL reports that the key is too small.  Well DUH! 521 bits does *SEEM* small, but isn't.
 
 #  Check if ./accountids.txt exists.  File format must be plaintext, and contain 1 12-character account nunber per line.
-if [ ! -f ./accountids.txt ]; then echo "./accountids.txt file not found.  Exiting...." && exit;
+if [ ! -s ./accountids.txt ]; then echo "./accountids.txt file not found.  Exiting...." && exit;
 fi
 
 #  Retreive Prisma Cloud write capable API credentials from 1Password 
@@ -15,14 +15,13 @@ ACCTGRPNAME="[Replace with name of the Account Group]"
 ACCTGRPID="[Replace with ID # of the Account Group]"
 USERNAME=$(awk 'BEGIN{FS="\""}{print$8}' <<< "${CREDS}")
 PASSWORD=$(awk 'BEGIN{FS="\""}{print$4}' <<< "${CREDS}")
-
-#  Authenticate againt Prisma Cloud.
-export PRISMAAPI=$(curl -k -s --request POST --url https://api.prismacloud.io/login --header 'Accept: application/json; charset=UTF-8' --header 'Content-Type: application/json; charset=UTF-8' --data '{"username":'"\"$USERNAME\""',"password":'"\"$PASSWORD\""'}' | awk 'BEGIN { FS="\"" } { print $4 }')
-
 #  Formats the input from ./accountids.txt to an array of stings.  This line can be commented out if ./accountids.txt is already in that fornat.
 #  Array of stirngs format is: "111111111111", "222222222222", "333333333333".  Quote are necessary in this format.
 ACCOUNTIDS=$(cat accountids.txt | sed -e 's/^/"/g' -e 's/$/"/g' | sed -z 's/\n/,\ /g' | sed -e 's/..$//')
 DESCRIPTION="Maintained by script. Manual updates may be overwritten"
+
+#  Authenticate againt Prisma Cloud.
+export PRISMAAPI=$(curl -k -s --request POST --url https://api.prismacloud.io/login --header 'Accept: application/json; charset=UTF-8' --header 'Content-Type: application/json; charset=UTF-8' --data '{"username":'"\"$USERNAME\""',"password":'"\"$PASSWORD\""'}' | awk 'BEGIN { FS="\"" } { print $4 }')
 
 #  The "description" and "name" require adding literal quotation marks surrounding the expanded variables". The other single and double quote syntax, when using variables is very cURL specific.
 #  Command should output a "200" status, else something went wrong.
