985338ee61779ba0361fdf2b9f8cf9158665c91b
andywoodt2 <90482206+andywoodt2@users.noreply.github.com>
2022-04-30 14:37:22 +0000

    Added some additional notes.

--- prisma_account_groups_update.sh
+++ prisma_account_groups_update.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-# This script will upadte an account group.  This process will overwrite the account group contents with the accounts in ./accountids.txt.
+#  This script will upadte an account group.  This process will overwrite the account group contents with the accounts in ./accountids.txt.
+#  I'm not a fan of using "-k" in the cURL commands, but hey, it's cURL, the certs are EC, and cURL reports that the key is too small.  Well DUH! 521 bits does *SEEM* small, but isn't.
 
-# Check if ./accountids.txt exists.  File format must be plaintext, and as written, 1 account per line.
+#  Check if ./accountids.txt exists.  File format must be plaintext, and contain 1 12-character account nunber per line.
 if [ ! -f ./accountids.txt ]; then echo "./accountids.txt file not found.  Exiting...." && exit;
 fi
 
 #  Retreive Prisma Cloud write capable API credentials from 1Password 
-ONEPWDACCT="[Replace with 1Password name or GUID]"
+ONEPWDACCT="[Replace with 1Password API credential name or GUID]"
+#  To note: While requesting the fields "username,credential", the order retreived will be "credential,username".
 CREDS=$(op get item $ONEPWDACCT --fields username,credential)
-ACCTGRPNAME="[Replace with name of Account Group]"
-ACCTGRPID="[Replace with Account Group ID #"
+ACCTGRPNAME="[Replace with name of the Account Group]"
+ACCTGRPID="[Replace with ID # of the Account Group]"
 USERNAME=$(awk 'BEGIN{FS="\""}{print$8}' <<< "${CREDS}")
 PASSWORD=$(awk 'BEGIN{FS="\""}{print$4}' <<< "${CREDS}")
 
-#Authenticate againt Prisma Cloud.
+#  Authenticate againt Prisma Cloud.
 export PRISMAAPI=$(curl -k -s --request POST --url https://api.prismacloud.io/login --header 'Accept: application/json; charset=UTF-8' --header 'Content-Type: application/json; charset=UTF-8' --data '{"username":'"\"$USERNAME\""',"password":'"\"$PASSWORD\""'}' | awk 'BEGIN { FS="\"" } { print $4 }')
 
-# Formats the input from ./accountids.txt to an array of stings.  This line can be commented out if ./accountids.txt is already in that fornat.
-# Array of stirngs format is: "111111111111", "222222222222", "333333333333".  Quote are necessary in this format.
+#  Formats the input from ./accountids.txt to an array of stings.  This line can be commented out if ./accountids.txt is already in that fornat.
+#  Array of stirngs format is: "111111111111", "222222222222", "333333333333".  Quote are necessary in this format.
 ACCOUNTIDS=$(cat accountids.txt | sed -e 's/^/"/g' -e 's/$/"/g' | sed -z 's/\n/,\ /g' | sed -e 's/..$//')
 DESCRIPTION="Maintained by script. Manual updates may be overwritten"
 
-
+#  The "description" and "name" require adding literal quotation marks surrounding the expanded variables". The other single and double quote syntax, when using variables is very cURL specific.
+#  Command should output a "200" status, else something went wrong.
 curl -w "%{http_code}\n" -k --request PUT \
   --url https://api.prismacloud.io/cloud/group/$ACCTGRPID \
   --header 'content-type: application/json' \
